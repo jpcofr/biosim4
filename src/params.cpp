2350f71231d4a816046f8082800820a1d20bec21
Juan Pablo Contreras Franco <juanp.contreras@gmail.com>
2025-10-25 06:34:07 +0200

    fix: use homebrew gnuplot path for macOS
    - Change hardcoded /usr/bin/gnuplot to /opt/homebrew/bin/gnuplot
    - Fixes 'gnuplot: No such file or directory' error on macOS
    - Linux users can override via graphLogUpdateCommand parameter in config

--- src/params.cpp
+++ src/params.cpp
@@ -63,7 +63,7 @@ void ParamManager::setDefaults()
     privParams.updateGraphLogStride = privParams.videoStride;
     privParams.deterministic = false;
     privParams.RNGSeed = 12345678;
-    privParams.graphLogUpdateCommand = "/usr/bin/gnuplot --persist ./tools/graphlog.gp";
+    privParams.graphLogUpdateCommand = "/opt/homebrew/bin/gnuplot --persist ./tools/graphlog.gp";
     privParams.parameterChangeGenerationNumber = 0;
 }
 
